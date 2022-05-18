fb54559a4b74a7df8e4bf489fcc36d38c049b760
huan <407722572@qq.com>
2022-05-18 12:04:26 +0800

    订单确认页面收货地址的增删改查实现

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -2,6 +2,11 @@
      <router-view></router-view>嵌入其他页面的核心代码 -->
 <template>
   <div>
+    <order-header :title="title">
+      <template v-slot:tip>
+        <span>{{tip}}</span>
+      </template>
+    </order-header>
     <router-view></router-view>
     <service-bar></service-bar>
     <nav-footer></nav-footer>
@@ -9,13 +14,36 @@
 </template>
 <script>
 // 加载组件
+import OrderHeader from '@/components/OrderHeader'
 import ServiceBar from '@/components/ServiceBar'
 import NavFooter from '@/components/NavFooter'
 export default {
   name: 'order',
   components: {
+    OrderHeader,
     ServiceBar,
     NavFooter
+  },
+  data() {
+    return {
+      title: '',
+      tip: ''
+    }
+  },
+  mounted() {
+    // order-header组件，根据路由地址，动态改变title和tip
+    // 获取当前页面路由地址
+    let path = this.$route.path
+    if (path == '/order/confirm') {
+      this.title = '确认订单'
+      this.tip = '请仔细核对收货地址'
+    } else if (path == '/order/list') {
+      this.title = '订单列表'
+      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多>'
+    } else if (path == '/order/pay') {
+      this.title = '支付订单'
+      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多>'
+    }
   }
 }
 </script>
