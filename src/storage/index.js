f177a84d2aad538d86b8b3ada2307d62319ecea2
huan <407722572@qq.com>
2022-08-23 23:49:35 +0800

    注释

--- src/storage/index.js
+++ src/storage/index.js
@@ -2,6 +2,7 @@
 const STORAGE_KEY = 'mall'
 export default {
   // 一个存储数据的例子:{user: {userName: "jack", age: 89, sex: 1}}
+  // 之所以对sessionStorage的API重新封装，是因为想把所有信息存储在一个对象mall里，数据更清晰
 
   // 存储值
   setItem(key, value, module_name) {
@@ -24,7 +25,7 @@ export default {
   getItem(key, module_name) {
     // 如果传入了第二个参数,module_name表示user,key表示username,即获取user对象里的username
     if (module_name) {
-      // this表示当前暴露的对象,getStorage是对象里封装的方法
+      // this表示当前暴露的对象,getItem是对象里封装的方法
       let val = this.getItem(module_name) //取user模块
       if (val) return val[key]
     }
@@ -37,7 +38,7 @@ export default {
     return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}')
   },
 
-  // 清空某一个值,可能时清空user模块,也可能是清空user模块下面的某个值
+  // 清空某一个值,可能是清空user模块,也可能是清空user模块下面的某个值
   clear(key, module_name) {
     // delete删除对象某个属性
     let val = this.getStorage() //先获取整个Storage数据
